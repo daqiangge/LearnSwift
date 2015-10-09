743ea62dd22296b6b07cb7ce93ae60e7b55b3c4b
liqiang <liqiang_wuxi@163.com>
2015-10-09 17:02:09 +0800

    2015-10-09-05-01
    2015-10-09-05-01
    常量和变量
    整数类型

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+//＝＝＝＝＝＝＝课时 2＝＝＝＝＝＝＝＝
+//＝＝＝＝＝＝常量和变量＝＝＝＝＝＝＝
+//＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+var str = "Hello, playground"
+print(str)
+
+let pi = 3.1415926
+print("\(pi)  \(str)")
+
+let str2 = "123123"
+let abc = str + str2
+print(abc)
+
+
+
+//＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+//＝＝＝＝＝＝＝课时 3＝＝＝＝＝＝＝＝
+//＝＝＝＝＝＝＝整数类型＝＝＝＝＝＝＝
+//＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+let minvalue = UInt8.min
+let maxvalue = UInt8.max
+
+let minvalue2 = Int.min
+let maxvalue2 = Int.max
+
+//在定义整形是推荐使用Int
+var a:Int = 2;
